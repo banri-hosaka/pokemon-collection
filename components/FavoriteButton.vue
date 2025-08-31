27e1c5a61c99d5df0f20e1d24b38262c4fab7f06
banri.hosaka <banri.hosaka@vitalize.co.jp>
2025-08-31 14:12:56 +0900

    feat: お気に入りボタン表示

---
+++ components/FavoriteButton.vue
@@ -0,0 +1,22 @@
+<!-- お気に入りボタン -->
+<template>
+  <a>♥</a>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const isFavorite = ref(false);
+
+function toggleFavorite() {
+  isFavorite.value = !isFavorite.value;
+}
+</script>
+
+<style scoped>
+a {
+  cursor: pointer;
+  text-decoration: none;
+  color: inherit;
+}
+</style>
